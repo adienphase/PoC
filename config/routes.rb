ee134e7459a3240cf7a3c3e79fa161fdb1ee2dc8
Aditya Kumar <adikumar@enphaseenergy.com>
2022-07-26 12:51:24 +0530

    signout link setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
